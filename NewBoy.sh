e761b1d39cb3b6787dcaea9ac1de7a8e5571c66b
JoetheManHowie <mightyjoe9@hotmail.com>
2019-10-15 08:42:32 -0700

    Cleaning up repo

--- NewBoy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-./Make_a_warhammer_PC.py
-
-./whatsNext.sh
-
--- new_party.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-touch Joe_PC2.txt Alex_PC2.txt Clay_PC2.txt Trevor_PC2.txt
-./Make_a_warhammer_PC.py > Joe_PC2.txt
-./Make_a_warhammer_PC.py > Alex_PC2.txt
-./Make_a_warhammer_PC.py > Clay_PC2.txt
-./Make_a_warhammer_PC.py > Trevor_PC2.txt
-./whatsNext.sh
